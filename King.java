99765a814acb15cf8d8a947f118c2641cfa981c8
Annika Ling <annikaling@Annikas-MacBook-Pro.local>
2023-05-20 12:14:45 -0700

    king comments added

--- King.java
+++ King.java
@@ -1,17 +1,25 @@
 import java.util.ArrayList;
 
+/**
+ * The king class handles the king chess pieces and their movement
+ */
 public class King extends ChessPiece {
-	
+
+	/**
+	 * King constructor to create a king object
+	 * @param file the alphabetical character corresponding to the column of the king
+	 * @param rank the number corresponding to the row that the king is on
+	 * @param side the side that the king is on
+	 */
 	public King(char file, int rank, String side) {
 		super("king", file, rank, side);
 	}
 	
-	/**
-	* 
-	* @param none
-	* @return kingMoves - ArrayList<String>
-	*/
 	
+	/**
+	 * This method returns an arraylist containing all the spaces that the king piece can move to
+	 * @return an arraylist containing all the spaces that the king can move to
+	 */
 	public ArrayList<String> getMoves() {
 		ArrayList<String> kingMoves = new ArrayList<String>();
 
