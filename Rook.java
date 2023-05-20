f6af04524094653698aaf4236f03e2e59e4d3cdf
Annika Ling <annikaling@Annikas-MacBook-Pro.local>
2023-05-20 12:35:14 -0700

    added rook comments

--- Rook.java
+++ Rook.java
@@ -1,11 +1,24 @@
 import java.util.ArrayList;
 
+/**
+ * The rook class handles the rook chess pieces and their movement
+ */
 public class Rook extends ChessPiece {
 
+	/**
+	 * Rook constructor to create a rook object
+	 * @param file the alphabetical character corresponding to the column of the rook
+	 * @param rank the number corresponding to the row that the rook is on
+	 * @param side the side that the rook is on
+	 */
 	public Rook(char file, int rank, String side) {
 		super("rook", file, rank, side);
 	}
 
+	/**
+	 * This method determines which spaces the rook can move to in front of the rook
+	 * @return an arraylist with the moves in front of the rook
+	 */
 	public ArrayList<String> getFrontSpaces() {
 		ArrayList<String> frontSpaces = new ArrayList<String>();
 
@@ -17,6 +30,10 @@ public class Rook extends ChessPiece {
 		return frontSpaces;
 	}
 
+	/**
+	 * This method determines which spaces the rook can move behind of the rook
+	 * @return an arraylist with the moves behind the rook
+	 */
 	public ArrayList<String> getBackSpaces() {
 		ArrayList<String> backSpaces = new ArrayList<String>();
 
@@ -28,6 +45,10 @@ public class Rook extends ChessPiece {
 		return backSpaces;
 	}
 
+    /**
+	 * This method determines which spaces the rook can move to the right of the rook
+	 * @return an arraylist with the moves to the right of the rook
+	 */
 	public ArrayList<String> getRightSpaces() {
 		ArrayList<String> rightSpaces = new ArrayList<String>();
 
@@ -39,6 +60,10 @@ public class Rook extends ChessPiece {
 		return rightSpaces;
 	}
 
+	/**
+	 * This method determines which spaces the rook can move to the left of the rook
+	 * @return an arraylist with the moves to the left of the rook
+	 */
 	public ArrayList<String> getLeftSpaces() {
 		ArrayList<String> leftSpaces = new ArrayList<String>();
 
@@ -50,6 +75,10 @@ public class Rook extends ChessPiece {
 		return leftSpaces;
 	}
 	
+	/**
+	 * This method returns an arraylist containing all the spaces that the rook piece can move to
+	 * @return an arraylist containing all the spaces that the rook can move to
+	 */
 	public ArrayList<String> getMoves() {
 		ArrayList<String> rookMoves = new ArrayList<String>();
 		rookMoves.addAll(getFrontSpaces());
