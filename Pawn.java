8e2e59e2760b1c1d9eca0b63d7816d827b84ca68
Annika Ling <annikaling@Annikas-MacBook-Pro.local>
2023-05-20 12:27:08 -0700

    added pawn comments

--- Pawn.java
+++ Pawn.java
@@ -1,20 +1,32 @@
 import java.util.ArrayList;
 
+/**
+ * The pawn class handles the pawn chess pieces and their movement
+ */
 public class Pawn extends ChessPiece {
 
+	/**
+	 * Pawn constructor to create a pawn object
+	 * @param file the alphabetical character corresponding to the column of the pawn
+	 * @param rank the number corresponding to the row that the pawn is on
+	 * @param side the side that the pawn is on
+	 */
 	public Pawn(char file, int rank, String side) {
 		super("pawn", file, rank, side);
 	}
 	
 	/**
 	* checks if the piece has moved and if it has it returns true
-	* @param none
-	* @return boolean
+	* @return true if the piece was moved, false if the piece was not moved
 	*/
 	public boolean hasMoved() {
 		return (isBlack() && getRank() != 2) || (isWhite() && getRank() != 7);
 	}
 
+	/**
+	 * This method returns an arraylist containing all the spaces that the pawn piece can move to
+	 * @return an arraylist containing all the spaces that the pawn can move to
+	 */
 	public ArrayList<String> getMoves() {
 		ArrayList<String> pawnMoves = new ArrayList<String>();
 
