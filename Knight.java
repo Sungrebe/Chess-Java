7af7033faf97d4722a313efbe68e1198d30d96e2
Annika Ling <annikaling@Annikas-MacBook-Pro.local>
2023-05-20 12:17:34 -0700

    added knight comments

--- Knight.java
+++ Knight.java
@@ -1,11 +1,24 @@
 import java.util.ArrayList;
 
+/**
+ * The knight class handles the knight chess pieces and their movement
+ */
 public class Knight extends ChessPiece {
 
+    /**
+	 * Knight constructor to create a knight object
+	 * @param file the alphabetical character corresponding to the column of the knight
+	 * @param rank the number corresponding to the row that the knight is on
+	 * @param side the side that the knight is on
+	 */
 	public Knight(char file, int rank, String side) {
 		super("knight", file, rank, side);
 	}
 
+    /**
+	 * This method returns an arraylist containing all the spaces that the knight piece can move to
+	 * @return an arraylist containing all the spaces that the knight can move to
+	 */
 	public ArrayList<String> getMoves() {
 		ArrayList<String> knightMoves = new ArrayList<String>();
 
