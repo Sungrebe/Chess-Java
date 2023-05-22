c4eecefe2a2f0235a64beee0be1d2ae824b9fafe
Ananth Ramaswamy <52579753+Sungrebe@users.noreply.github.com>
2023-05-21 21:26:07 -0700

    Update ChessApp.java
    Final working version

--- ChessApp.java
+++ ChessApp.java
@@ -16,12 +16,35 @@ class ChessApp extends JFrame {
         setLayout(new BorderLayout());
         add(new Board(), BorderLayout.CENTER);
     }
+    
+    /**
+      * Recursive countdown (fun way to load the chess app GUI)
+      * @param n - start of the countdown
+    */
+    public static void countdown(int n) {
+        System.out.println(n);
+        
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
+        if (n > 1) {
+            countdown(n - 1);
+        } else {
+            System.out.println(0);
+        }
+    }
 
     /**
      * The main method that runs the game
      * @param args the arguments
      */
     public static void main(String[] args) {
+        System.out.println("Loading...");
+        countdown(10);
+        
         ChessApp cApp = new ChessApp("Java Chess Game");
         cApp.setSize(400, 428);
         cApp.setResizable(false);
